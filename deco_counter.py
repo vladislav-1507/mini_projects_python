d8fcc423b31da80e55d092a0d8f00e38fe8f5c58
Vladislav Krasnokutskii <vlad150789@mail.ru>
2025-09-01 16:04:12 +0300

    комменты

--- deco_counter.py
+++ deco_counter.py
@@ -1,20 +1,29 @@
 import functools
 
+
+# Декоратор, который будет считать вызовы функции
 def counter(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+        # СТРОКА wrapper.num += 1: Выполняется при КАЖДОМ вызове декорированной функции.
+        # Увеличивает атрибут 'num', который "прикреплен" к самой функции wrapper.
         wrapper.num += 1
-        print(f'Вызов {func.__name__}: {wrapper.num}')
+        print(f"Вызов {func.__name__}: {wrapper.num}")
         val = func(*args, **kwargs)
         return val
+
+    # СТРОКА wrapper.num = 0: Выполняется ОДИН РАЗ, когда декоратор применяется к функции.
+    # Создает и инициализирует атрибут 'num' на объекте функции wrapper.
     wrapper.num = 0
     return wrapper
 
+
 @counter
 def greet(name):
-    return f'Hello {name}!'
+    return f"Hello {name}!"
 
-print(greet('Timur'))
-print(greet('Ruslan'))
-print(greet('Arthur'))
-print(greet('Gvido'))
+
+print(greet("Timur"))
+print(greet("Ruslan"))
+print(greet("Arthur"))
+print(greet("Gvido"))
